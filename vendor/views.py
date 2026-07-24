4e2dc2db37da23c3fddc615070150350bc3e673e
Sujan <sujanghimire002@gmail.com>
2026-07-24 22:43:15 +0545

    creating a footer, messages, navbar, pagination and sidebar templates inside the includes folder of vendor app inside templates folder.

--- vendor/views.py
+++ vendor/views.py
@@ -3,4 +3,4 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class HomeView(TemplateView):
-    template_name = "vendor/base.html"
+    template_name = "Vendor/base.html"
