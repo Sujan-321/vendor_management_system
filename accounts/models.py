4b6c3638a4a6a47f59f347474762373a4a7cb522
Sujan <sujanghimire002@gmail.com>
2026-07-26 20:30:23 +0545

    creating a user model in accounts app.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+phone_validator = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Enter a valid phone number.")
+
+
+class User(AbstractUser):
+
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        VENDOR = "VENDOR", "Vendor"
+        CUSTOMER = "CUSTOMER", "Customer"
+
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, validators=[phone_validator])
+    profile_image = models.ImageField(upload_to="profiles/", blank=True, null=True)
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.CUSTOMER)
+    is_verified = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS = ["email"]
+
+    class Meta:
+        ordering = ["username"]
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.username
