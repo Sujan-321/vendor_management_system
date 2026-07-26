c57f3de374fd678023c1456a4277998901632431
Sujan <sujanghimire002@gmail.com>
2026-07-26 20:56:10 +0545

    creating a views for accounts app. views including loginview, registerview, profileview, profileupdateview and changepasswordview.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class LoginView(TemplateView):
+    template_name = "accounts/login.html"
+
+
+class RegisterView(TemplateView):
+    template_name = "accounts/register.html"
+
+
+class ProfileView(TemplateView):
+    template_name = "accounts/profile.html"
+
+
+class ProfileUpdateView(TemplateView):
+    template_name = "accounts/profile_update.html"
+
+
+class ChangePasswordView(TemplateView):
+    template_name = "accounts/change_password.html"
