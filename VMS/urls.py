5838357107edffb7feeadc892f33a59a8ee51d74
Sujan <sujanghimire002@gmail.com>
2026-07-27 23:10:08 +0545

    creating css files and script files inside the accounts app.

--- VMS/urls.py
+++ VMS/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('', include('accounts.urls', namespace="accounts")),
+    path('accounts/', include('accounts.urls', namespace="accounts")),
     path('vendor/', include('vendor.urls')),
 ]
