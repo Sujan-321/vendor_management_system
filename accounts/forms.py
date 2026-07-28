d8fb7c53d4f411d1e3675bafda9dc9f92dffc4d2
Sujan <sujanghimire002@gmail.com>
2026-07-28 22:47:34 +0545

    update the registrationform by displaying the placeholder in the template.

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,20 +6,63 @@ from .models import User
 
 class RegistrationForm(UserCreationForm):
     ROLE_CHOICES = (
-        ('Customer', 'Customer'),
-        ('Vendor', 'Vendor'),
+        ("Customer", "Customer"),
+        ("Vendor", "Vendor"),
     )
+    
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "johndoe",
+        })
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            "class": "form-control",
+            "placeholder": "you@example.com",
+        })
+    )
+
+    phone_number = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "+977 9812345678",
+        })
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "class": "form-control",
+            "placeholder": "••••••••",
+        })
+    )
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "class": "form-control",
+            "placeholder": "••••••••",
+        })
+    )
+
 
     role = forms.ChoiceField(
         choices=ROLE_CHOICES,
-        widget=forms.Select(attrs={"class":"form-select"})
+        widget=forms.Select(attrs={
+            "class": "form-select"
+        })
     )
 
-    agree_terms = forms.BooleanField(required=True)
+    agree_terms = forms.BooleanField(
+        widget=forms.CheckboxInput(attrs={
+            "class": "form-check-input"
+        })
+    )
 
     class Meta:
         model = User
-        fields=(
+        fields = (
             "username",
             "email",
             "phone_number",
