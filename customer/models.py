4dc24d072f6b085bfd4db9d6c35131feab8858ee
Sujan <sujanghimire002@gmail.com>
2026-08-03 23:05:34 +0545

    creating a orderitem in the customer model.

--- customer/models.py
+++ customer/models.py
@@ -179,3 +179,32 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_number
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="items"
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    quantity = models.PositiveIntegerField()
+
+    price = models.DecimalField(
+        max_digits=12,
+        decimal_places=2
+    )
+
+    subtotal = models.DecimalField(
+        max_digits=12,
+        decimal_places=2
+    )
+
+    def __str__(self):
+        return self.product.name
