dde397fe9080f419e1b71f5c73a33cf65f2f8cf5
Sujan <sujanghimire002@gmail.com>
2026-08-05 21:05:37 +0545

    register all models in admin py of vendor and customer app.

--- customer/admin.py
+++ customer/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, Cart, CartItem, Wishlist, ShippingAddress, Order, OrderItem, Payment, ProductReview
 # Register your models here.
 
 
 admin.site.register(Customer)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Wishlist)
+admin.site.register(ShippingAddress)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Payment)
+admin.site.register(ProductReview)
--- vendor/admin.py
+++ vendor/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Vendor
+from .models import Vendor, Category, Product, ProductImage, ProductSpecification, Coupon
 
 admin.site.register(Vendor)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(ProductSpecification)
+admin.site.register(Coupon)
