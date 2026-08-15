812ea9d6dd182f659f73a9052f0b0bd41675d2fa
Sujan <sujanghimire002@gmail.com>
2026-08-15 09:29:46 +0545

    I have changed the admin app into adminportal this is the custom app because django admin conflict the same name.

--- VMS/settings.py
+++ VMS/settings.py
@@ -46,9 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'admin',
     'vendor',
     'customer',
+    'adminportal'
 ]
 AUTH_USER_MODEL = "accounts.User"
 
