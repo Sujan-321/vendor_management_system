c10c9bbeab2f04b7fbee40d55eeb791a288b48df
Sujan <sujanghimire002@gmail.com>
2026-08-20 13:42:05 +0545

    update the ALLOWED_HOSTS in settings.py file.

--- VMS/settings.py
+++ VMS/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY")
 # DEBUG = True
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
 
 # Application definition
